58e3a7c8361bfb52890f864109454badcd1832a7
vtobias10 <tobivelazquez80@gmail.com>
2024-02-17 12:04:13 -0300

    primer commit

---
+++ js/main.js
@@ -0,0 +1,55 @@
+const body = document.body;
+const darkModeToggle = document.getElementById('darkModeToggle');
+const lightModeIcon = document.querySelector('.light-mode-icon');
+const darkModeIcon = document.querySelector('.dark-mode-icon');
+
+document.addEventListener('DOMContentLoaded', () => {
+    const currentMode = localStorage.getItem('darkMode');
+
+    if (currentMode === 'dark') {
+        enableDarkMode();
+    }
+
+    darkModeToggle.addEventListener('click', () => {
+        if (body.classList.contains('dark-mode')) {
+            disableDarkMode();
+        } else {
+            enableDarkMode();
+        }
+    });
+});
+
+function enableDarkMode() {
+    body.classList.add('dark-mode');
+    lightModeIcon.style.opacity = '0';
+    darkModeIcon.style.opacity = '1';
+    localStorage.setItem('darkMode', 'dark');
+}
+
+function disableDarkMode() {
+    body.classList.remove('dark-mode');
+    lightModeIcon.style.opacity = '1';
+    darkModeIcon.style.opacity = '0';
+    localStorage.setItem('darkMode', 'light');
+}
+
+
+function enviarCorreo() {
+    var destinatario = 'tobivelazquez80@gmail.com';
+            var asunto = 'Asunto del correo';
+    var cuerpo = 'Cuerpo del correo';
+
+    var enlaceCorreo = 'mailto:' + destinatario + '?subject=' + encodeURIComponent(asunto) + '&body=' + encodeURIComponent(cuerpo);
+
+    window.location.href = enlaceCorreo;
+}
+
+function copiarAlPortapapeles() {
+    var campoEntrada = document.getElementById('floatingInputDisabled');
+            
+    ampoEntrada.select();
+            
+    document.execCommand('copy');
+            
+    campoEntrada.setSelectionRange(0, 0);
+}
